4ce1648ef06e73bbfa24dc6b47e7a475f0146366
Jacob <jakeboone@aol.com>
2016-02-15 15:48:12 -0500

    working contact form

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'contact' });
+  res.render('contact', { title: 'Contact' });
 });
+
+router.post('/send', function(req, res, next){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'onyxholisticservices@gmail.com',
+			pass: 'xjbjxjvtbtitwmtt'
+		}
+	})
+
+
+	
+
+
+    var mailOptions = {
+        from: '<onyxholisticservices@gmail.com>',
+        to: 'onyxholisticservices@gmail.com',
+        subject: 'Contact Form',
+        text: 'Name: ' + req.body.name + '\n' + 'Email: ' + req.body.email + '\n' + req.body.message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+    	        if (error) {
+            res.render('contact', {
+                title: 'Contact Error',
+                msg: 'Error occured, message not sent.',
+                err: true,
+                page: 'contact'
+            });
+        } else {
+            res.render('contact', {
+                title: 'Contact Sent...',
+                msg: 'Message sent! Thank you.',
+                err: false,
+                page: 'contact'
+            });
+        }
+
+    });
+
+
+});
+
+
+
+
 
 module.exports = router;
